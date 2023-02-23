4dc4cbc69f83c6364af0ef8383dda9304d67bc87
hs <aegean>
2023-02-23 11:27:17 +0800

    1

---
+++ json.go
@@ -0,0 +1,46 @@
+package helpers
+
+import "encoding/json"
+
+/**
+ * @Description: Json转换助手
+ * @Date: 2022-08-26 15:25:35
+ */
+func JsonMapToString(data map[string]interface{}) string {
+	if len(data) <= 0 {
+		return ""
+	}
+	jsonObj, _ := json.Marshal(data)
+	return string(jsonObj)
+}
+
+func JsonSliceToString(data []string) string {
+	if len(data) <= 0 {
+		return ""
+	}
+	jsonObj, _ := json.Marshal(data)
+	return string(jsonObj)
+}
+
+func JsonToString(data interface{}) string {
+	jsonObj, _ := json.Marshal(data)
+	return string(jsonObj)
+}
+
+func JsonDecodeToMap(data string) map[string]interface{} {
+	var res map[string]interface{}
+	json.Unmarshal([]byte(data), &res)
+	return res
+}
+
+func JsonDecodeToSlice(data string) []string {
+	var res []string
+	json.Unmarshal([]byte(data), &res)
+	return res
+}
+
+func JsonDecodeToSliceMap(data string) []map[string]interface{} {
+	var res []map[string]interface{}
+	json.Unmarshal([]byte(data), &res)
+	return res
+}
